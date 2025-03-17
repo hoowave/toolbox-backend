cccc7630960248ed421ef83780804326858b5f7f
장상훈 <jangsanghun@MacBookPro.lan>
2025-03-17 17:36:53 +0900

    fix origin

--- src/main/java/com/tools/toolbox/config/security/CorsConfig.java
+++ src/main/java/com/tools/toolbox/config/security/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://dev.hoowave.org"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
